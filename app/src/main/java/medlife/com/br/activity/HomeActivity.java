683497be925c7617f120718fa73b7be28f14c4ed
eduwill <eduwill100@gmail.com>
2024-07-23 19:14:31 -0300

    Initial Commit

---
+++ app/src/main/java/medlife/com/br/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package medlife.com.br.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import medlife.com.br.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
