3ad28b940e6117099a55902c2a305d57e1a1ae02
eduwill <eduwill100@gmail.com>
2025-06-02 00:26:01 -0300

    Esconder a ActionBar na AutenticacaoActivity
    - Na AutenticacaoActivity.java, a ActionBar agora é ocultada usando `getSupportActionBar().hide()`.

--- app/src/main/java/medlife/com/br/activity/AutenticacaoActivity.java
+++ app/src/main/java/medlife/com/br/activity/AutenticacaoActivity.java
@@ -45,6 +45,7 @@ public class AutenticacaoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autenticacao);
+        getSupportActionBar().hide(); // Hide action bar if needed
 
         inicializaComponentes();
 //        configurarGoogleSignIn();
